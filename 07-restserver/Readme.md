665f3cf6f098e777264618f7b1d67d30feec6cdb
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-07-24 12:34:57 -0600

    agregado proyecto de restserver

---
+++ 07-restserver/Readme.md
@@ -0,0 +1,14 @@
+# RestServer
+
+Comando para ejecutar
+
+```
+    node server/server
+    nodemon server/server
+```
+
+Paquetes Utilizados
+
+1. [Express](https://www.npmjs.com/package/express) Para el server
+
+2. [Body Parser](https://www.npmjs.com/package/body-parser) Para manejar los formularios
---
+++ 07-restserver/server/server.js
@@ -0,0 +1,50 @@
+require("./config/config");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+//configuracion para el body parser
+// middleware aplicado para capturar lo del formulario
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+/*
+app.get("/", function(req, res) {
+    // send es para enviar html
+    // json es para enviar json
+    res.json("Hello World");
+});*/
+app.get("/usuario", function(req, res) {
+    res.json("get Usuario");
+});
+
+app.post("/usuario", function(req, res) {
+    let body = req.body;
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: "El nombre es necesario",
+        });
+    } else {
+        res.json({
+            persona: body,
+        });
+    }
+});
+
+app.put("/usuario/:id", function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id,
+    });
+});
+
+app.delete("/usuario", function(req, res) {
+    res.json("delete Usuario");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Escuchando puerto: ", process.env.PORT);
+});
