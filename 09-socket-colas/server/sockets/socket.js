3a2b5ac03b58210653c002320f2eed0b247314da
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-07-28 17:35:10 -0600

    finalizado proyecto de sistema de colas

--- 09-socket-colas/server/sockets/socket.js
+++ 09-socket-colas/server/sockets/socket.js
@@ -1,42 +1,37 @@
-const { io } = require('../server');
+const { io } = require("../server");
+const { TicketControl } = require("../classes/ticket-control");
 
+const ticketControl = new TicketControl();
 
-io.on('connection', (client) => {
-
-    console.log('Usuario conectado');
-
-    client.emit('enviarMensaje', {
-        usuario: 'Administrador',
-        mensaje: 'Bienvenido a esta aplicación'
+io.on("connection", (client) => {
+    console.log("Usuario conectado");
+    // Para ver el ticket siguiente por medio del boton
+    client.on("siguienteTicket", (data, callback) => {
+        let siguiente = ticketControl.siguiente();
+        console.log(siguiente);
+        callback(siguiente);
     });
 
-
-
-    client.on('disconnect', () => {
-        console.log('Usuario desconectado');
+    client.emit("estadoActual", {
+        actual: ticketControl.getEstadoTicket(),
+        ultimos4: ticketControl.getUltimos4(),
     });
 
-    // Escuchar el cliente
-    client.on('enviarMensaje', (data, callback) => {
+    client.on("atenderTicket", (data, callback) => {
+        //console.log(data.escritorio);
 
-        console.log(data);
-
-        client.broadcast.emit('enviarMensaje', data);
-
-
-        // if (mensaje.usuario) {
-        //     callback({
-        //         resp: 'TODO SALIO BIEN!'
-        //     });
-
-        // } else {
-        //     callback({
-        //         resp: 'TODO SALIO MAL!!!!!!!!'
-        //     });
-        // }
-
-
-
+        if (!data.escritorio) {
+            return callback({
+                err: true,
+                mensaje: "El escritorio es necesario",
+            });
+        }
+        let atenderTicket = ticketControl.atenderTicket(data.escritorio);
+        callback(atenderTicket);
+        //actualizar / notificar cambiosm en los ultimos 4
+        // evento que debe de dispararse dentro de atender ticket
+        client.broadcast.emit("ultimos4", {
+            ultimos4: ticketControl.getUltimos4(),
+        });
     });
-
 });
